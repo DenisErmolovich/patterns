0073e8f775d226310e6123f4fd3f4bc0e4cb06bf
DenisErmolovich <denisermolovich@fico.com>
2019-04-02 12:59:26 +0300

    Add composite pattern

---
+++ src/composite/Composite.java
@@ -0,0 +1,20 @@
+package composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Composite implements Shape {
+    private List<Shape> components = new ArrayList<>();
+
+    public void addComponent(Shape component) {
+        components.add(component);
+    }
+    public void removeComponent(Shape component) {
+        components.remove(component);
+    }
+
+    @Override
+    public void draw() {
+        components.forEach(component -> component.draw());
+    }
+}
